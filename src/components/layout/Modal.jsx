0454395dce317188c33a02f76849a655590acc7c
stefonsimmons <s.j.simmons@spartans.nsu.edu>
2023-05-05 15:54:47 -0400

    update some moile design

--- src/components/layout/Modal.jsx
+++ src/components/layout/Modal.jsx
@@ -1,12 +1,12 @@
-import { Link } from "react-router-dom"
+import { Link, useLocation } from "react-router-dom"
 
 export default function Modal(props) {
-
+  const location = useLocation()
 
   return (
     <div className={`modal ${props.openModal ? 'open' : 'close'}`}>
       <header className='modal-header'>
-          <h1>stefon_simmons</h1>
+          <h1>{location.pathname.slice(1).replace('-','_')}</h1>
         <Link to="/">
           <button onClick={() => props.setModal(false)} className="modal-close">
             X
